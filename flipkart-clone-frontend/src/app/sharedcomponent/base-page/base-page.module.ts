a28d55c04548b2b894ac057c6c36baf651ab082c
ShubhamGargade <gargadeshubham@gmail.com>
2023-05-21 17:00:28 +0530

    Initial Config

---
+++ flipkart-clone-frontend/src/app/sharedcomponent/base-page/base-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasePageComponent } from './base-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    BasePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BasePageModule { }
